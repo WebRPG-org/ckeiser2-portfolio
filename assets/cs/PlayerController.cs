3a3f4cff53e36a0d8de2d241f03c76cfc9173230
ckeiser2 <145719050+ckeiser2@users.noreply.github.com>
2026-03-17 22:15:09 +0000

    Deploying to gh-pages from @ ckeiser2/portfolio@9cd122005eb3d7265b228f5c3c1bd355828eb835 🚀

---
+++ assets/cs/PlayerController.cs
@@ -0,0 +1,86 @@
+// In progress PlayerController.cs script handles player movement and physics and contact with enemies.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Count variable for UI to keep track of collectibles
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+
+        winTextObject.SetActive(false);
+
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 13)
+        {
+            winTextObject.SetActive(true);
+
+            // Destroy enemy
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.CompareTag("Enemy"))
+       {
+           // Destroy the current object
+           Destroy(gameObject); 
+           
+           // Update the winText to display "You Lose!"
+           winTextObject.gameObject.SetActive(true);
+           winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+       }
+    }
+
+    // On contact will disable game objects with "PickUp" tag. Aka our collectibles in the demo.
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+        
+    }
+    
+}

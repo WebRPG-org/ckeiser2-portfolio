5c89df98d901b6800478b7af697fd4ae15f8039d
Colton Keiser <colton.dev02@gmail.com>
2026-03-30 10:02:44 -0500

    update to u2 proj

---
+++ assets/cs/U2/Obstacle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    public float maxSpinSpeed = 10f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        // Speed is effected by size of asteroid
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+
+        Vector2 randomDirection = Random.insideUnitCircle;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(randomDirection * randomSpeed);
+
+        // Asteroid objects rotate randomly
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
+    }
+}

831a90b2158a4db0b7f2e0c05cb0fb39f97178d5
ckeiser2 <145719050+ckeiser2@users.noreply.github.com>
2026-04-10 16:31:23 +0000

    Deploying to gh-pages from @ ckeiser2/portfolio@d4e8db3fa371dc14454da8525237aaa539832f8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "A look at the repo behind this page and my other github projects. *Note* my previous portfolio page (ckeiser2) has over 200 commits to it over the past 2 years of development. But, since I pushed those updates from my IDE with a different email than my github, it did not count as a contribution.",
+          description: "A look at the repo behind this page and my other github projects. Stats are pulled by a javascript repo and hosted on Vercel for real time pulling of data.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio/repositories/";
